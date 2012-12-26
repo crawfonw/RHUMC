afab65113ba6cb88ac2b20ca2768f07e35fb65e4
Nick Crawford <crawfonw@rose-hulman.edu>
2012-12-26 00:43:00 -0500

    Some initial models; edit gitignore

--- RHITUMC/conference/models.py
+++ RHITUMC/conference/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User, UserManager
 from django.db import models
 
-# Create your models here.
+class UMCUser(User):
+    school = models.CharField(max_length=100)
+    
+    objects = UserManager()
+
+class Conference(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField(help_text='yyyy-mm-dd')
+    end_date = models.DateField(help_text='yyyy-mm-dd')
+    
+    participants = models.ManyToManyField(UMCUser)
+    
+    def __unicode__(self):
+        return self.name
