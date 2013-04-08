999a5d8a7b9991c335627226c58966d56d01a092
Nick Crawford <crawfonw@rose-hulman.edu>
2013-04-08 12:23:39 -0400

    Fix #21

--- RHITUMC/RHITUMC/urls.py
+++ RHITUMC/RHITUMC/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.http import HttpResponse
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^', include('conference.urls')),
     
     url(r'^favicon.ico', 'conference.views.index'),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
     
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls), name='admin'),
