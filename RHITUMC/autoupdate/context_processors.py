70e1dabdf5299d33f65e6a78497d1bbb35e262c0
Nick Crawford <n.crawfish@gmail.com>
2014-01-11 14:47:06 -0500

    Add license to python files; add update checking based on time passed; add update type to json file
    Still need to add grabbing from url

--- RHITUMC/autoupdate/context_processors.py
+++ RHITUMC/autoupdate/context_processors.py
@@ -1,19 +1,48 @@
-'''
-Created on Jan 10, 2014
+"""
+    Conference scheduling software for undergraduate institutions.
+    Created specifically for the Mathematics department of the
+    Rose-Hulman Institute of Technology (http://www.rose-hulman.edu/math.aspx).
+    
+    Copyright (C) 2013-2014  Nick Crawford <crawfonw -at- gmail.com>
 
-@author: nick
-'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+import datetime
 import importlib
 
 from utils import get_all_modules
 
+UPDATE_PATHS = ['admin', 'portal'] #where it will check and notify
+
+#We'll just store this in memory. If the program gets restarted,
+#then it doesn't hurt to check.
+last_check = None
+
 def autoupdate_cp(request):
-    if request.method == 'GET' and 'admin' in request.get_full_path():
-        for module_str in get_all_modules():
-            try:
-                module = importlib.import_module(module_str)
-                print "'%s' version: %s" % (module_str, module.VERSION)
-            except ImportError:
-                print "'%s' version missing or specified incorrectly; skipping." % module_str
+    global last_check
+    print 'Last update check: ', last_check
+    if request.method == 'GET' and \
+        len([i for i in UPDATE_PATHS if i in  request.get_full_path()]) > 0:
+        if last_check is None or last_check >= datetime.datetime.now() + \
+            datetime.timedelta(weeks=1):
+            for module_str in get_all_modules():
+                try:
+                    module = importlib.import_module(module_str)
+                    print "'%s' version: %s" % (module_str, module.VERSION)
+                except ImportError:
+                    print "'%s' version missing or specified incorrectly; skipping." % module_str
+            last_check = datetime.datetime.now()
     return {}
